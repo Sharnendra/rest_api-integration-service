4dcbdf756040fd7e5c3b40b1894ee2688740dca3
767967 <767967@PC300680.cts.com>
2019-05-09 12:50:28 +0530

    new Architechture

--- src/main/java/com/service/integration/controller/IntegrationController.java
+++ src/main/java/com/service/integration/controller/IntegrationController.java
@@ -1,15 +1,29 @@
 package com.service.integration.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.service.integration.config.CommonConfigurations;
+import com.service.integration.config.ConfigService;
 
 @RestController
 public class IntegrationController {
 	
+	@Autowired
+	private ConfigService configService;
+	
+	
 	@RequestMapping("/header")
-	public String checkHeader(@RequestHeader(value="service_type",required=true) String service_type) {
-		return "Hearder Requred";
+	public @ResponseBody CommonConfigurations checkHeader(@RequestHeader(value="service_type",required=true) String service_type) {
+		CommonConfigurations commonConfigurations=null;
+		if(configService.getServicemapper().containsKey(service_type))
+		{
+			commonConfigurations=configService.getServicemapper().get(service_type);
+		}
+		return commonConfigurations;
 	}
 
 }
