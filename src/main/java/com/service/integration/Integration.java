506454b3940d1a8f81e210cc2f85e9750aa8c7e4
767967 <767967@PC300680.cts.com>
2019-05-13 12:42:21 +0530

    HRSS APP SPRING BOOT

--- src/main/java/com/service/integration/Integration.java
+++ src/main/java/com/service/integration/Integration.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Integration {
-
+//HRSS APP
 	public static void main(String[] args) {
 		SpringApplication.run(Integration.class, args);
 	}
